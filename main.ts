7f0920dbad5c6d258cfd74e2c61648b36bf1c507
robot-ie <mig.ruiz+1@gmail.com>
2024-08-22 20:24:01 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+namespace variables {
+
+
+
+  
+
+    //% block
+    //% group="Basic"
+    export function onObjectDetected(pin:DigitalPin, handler: () => void) {
+        pins.setPull(pin, PinPullMode.PullUp)
+        let lastState = 1
+        let currentState = 1
+        pins.setAudioPinEnabled(false)
+        basic.forever(function () {
+            currentState = pins.digitalReadPin(pin)
+            if (lastState != currentState && currentState == 0) {
+                handler()
+            }
+            lastState = currentState
+        })
+    }
+
+    let strip:neopixel.Strip = null
+    //% block
+    //% group="Basic"
+    export function turnLights(pin: DigitalPin, lightsNumber:number) {
+        if (strip==null){
+            strip = neopixel.create(pin, 9, NeoPixelMode.RGB)
+        }
+        let range = strip.range(0, lightsNumber)
+        strip.clear()
+        strip.show()
+        range.showColor(NeoPixelColors.Red)
+        
+    }
+
+
+  
+    //% block
+    //% group="Basic"
+    export function animateLights(pin: DigitalPin) {
+        if (strip == null) {
+            strip = neopixel.create(pin, 9, NeoPixelMode.RGB)
+        }
+        for (let i = 0; i < 5; i++) {
+            strip.showColor(NeoPixelColors.Red)
+            basic.pause(100)
+            strip.clear()
+            strip.show()
+            basic.pause(100)
+        }
+        
+
+    }
+
+
+
+
+}
+
+
+
